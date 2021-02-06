7266899c0170097af7c353d926ac1841f3a67498
Nathan Gilbert <nathan.gilbert@gmail.com>
2021-02-06 10:58:09 -0700

    adding cell tests

---
+++ tests/test_cell.py
@@ -0,0 +1,23 @@
+import unittest
+from conway.cell import Cell
+
+
+class TestCell(unittest.TestCase):
+
+    def test_is_alive(self):
+        c = Cell()
+        self.assertFalse(c.is_alive())
+        c.set_alive()
+        self.assertTrue(c.is_alive())
+
+    def test_get_print_character(self):
+        c = Cell()
+        self.assertEqual(c.get_print_character(), '.')
+        c.set_alive()
+        self.assertEqual(c.get_print_character(), '0')
+        c.set_dead()
+        self.assertEqual(c.get_print_character(), '.')
+
+
+if __name__ == '__main__':
+    unittest.main()
